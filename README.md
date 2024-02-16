f9c668171beaf4364c245349ca30567dbe87a2f7
Victor Jong <victor.jong@ncs.com.sg>
2024-02-17 00:18:32 +0800

    update test & readme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Getting Started with Create React App | [Live Site](https://victorjongsoon.github.io/isit207-petheaven/)
+# Pet Heaven (React App) | [Live Site](https://victorjongsoon.github.io/isit207-petheaven/)
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders with correct title', () => {
+  render(
+    <MemoryRouter initialEntries={['/']} basename="/isit207-petheaven">
+      <App />
+    </MemoryRouter>
+  );
 });
